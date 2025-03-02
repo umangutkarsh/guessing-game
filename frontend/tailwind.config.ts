0a9744bec9c217219458cff417ca0b338a13f1e5
Umang Utkarsh <umangutkarsh0024@gmail.com>
2025-03-02 09:25:00 +0530

    frontend mods

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -5,7 +5,7 @@ module.exports = {
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
 		// If you're using the src directory:
-		'./src/**/*.{js,ts,jsx,tsx}',
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
 		extend: {},
